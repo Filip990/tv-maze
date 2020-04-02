13c93251a1e7dfc68f673717b794b3af2a33b666
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-04-02 16:59:48 +0200

    added redux and applied a first reducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
 
 import Home from "./components/HomeComponent/HomeComponent";
-
 import "./App.css";
 
-function App() {
-  useEffect(() => {
-    const fetchTvShows = async () => {
-      try {
-        const res = await fetch("http://api.tvmaze.com/shows");
-        const data = await res.json();
-        console.log(data);
-      } catch (err) {}
-    };
+import tvShowsReducer from "./components/HomeComponent/tvShowsReducer";
 
-    fetchTvShows();
-  }, []);
+function App() {
+  const store = createStore(
+    combineReducers({
+      allShows: tvShowsReducer
+    }),
+    applyMiddleware(thunk)
+  );
+
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
