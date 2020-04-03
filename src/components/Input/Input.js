ed16d5571947cd551f3d3a6e18bce496e8e37c84
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-04-03 17:34:20 +0200

    added debounce

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import "./Input.css";
+import { debounce } from "lodash";
 
+import "./Input.css";
 import { searchAllTvShows } from "./inputActionCreator";
 
 const Input = props => {
   const dispatch = useDispatch();
 
+  const debouncedFn = debounce(value => {
+    dispatch(searchAllTvShows(value));
+  }, 300);
+
   const search = event => {
     const { value } = event.target;
-    setTimeout(() => {
-      dispatch(searchAllTvShows(value));
-    }, 500);
+    debouncedFn(value);
   };
 
   return <input onChange={search} placeholder={props.placeholder} />;
