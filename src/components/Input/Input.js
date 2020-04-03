87bf65976610547bb2867053711ab75e1df192bd
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-04-03 20:56:31 +0200

    made input a reusable component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { debounce } from "lodash";
 
 import "./Input.css";
-import { searchAllTvShows } from "./searchActionCreator";
 
 const Input = props => {
-  const dispatch = useDispatch();
-
-  const debouncedFn = debounce(val => {
-    dispatch(searchAllTvShows(val));
-  }, 300);
-
-  const search = event => {
-    const { value } = event.target;
-    debouncedFn(value);
-  };
-
-  return <input onChange={search} placeholder={props.placeholder} />;
+  return <input onChange={props.onChange} placeholder={props.placeholder} />;
 };
 
 export default Input;
