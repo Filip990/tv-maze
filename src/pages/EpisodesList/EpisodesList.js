c7c9dd679b339849efb9fa486a86bb7ac1f9257b
Filip Filipovic <filipfilipovic.stray@gmail.com>
2020-04-13 18:11:53 +0200

    added custom hook for fetching data

--- src/pages/EpisodesList/EpisodesList.js
+++ src/pages/EpisodesList/EpisodesList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
 import { formatDate, stripHtmlFromString } from "../../utils/helperFunctions";
@@ -6,27 +6,14 @@ import placeholderImg from "../../assets/no_image.jpg";
 import { Episodes, EpisodeListItem, Summary } from "./EpisodesList.styled";
 
 import Spinner from "../../components/Spinner/Spinner";
+import useFetchRemoteData from "../../utils/useFetchRemoteData";
 
 const EpisodesList = () => {
   const { id } = useParams();
-  const [episodes, setEpisodes] = useState([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const getEpisodesList = async (id) => {
-      try {
-        const res = await fetch(`http://api.tvmaze.com/seasons/${id}/episodes`);
-        const episodesList = await res.json();
-        setEpisodes(episodesList);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-
-    getEpisodesList(id);
-  }, [id]);
+  const { response, error, isLoading } = useFetchRemoteData(
+    `http://api.tvmaze.com/seasons/${id}/episodes`
+  );
 
   return (
     <>
@@ -34,19 +21,21 @@ const EpisodesList = () => {
         <Spinner />
       ) : (
         <Episodes>
-          {episodes.map((episode) => (
-            <EpisodeListItem key={episode.id}>
-              <div>
-                <h3> {episode.name}</h3>
-                <p>Official Air Date: {formatDate(episode.airdate)}</p>
-                <p>Runtime: {episode.runtime} minutes</p>
-                <img src={episode.image?.medium || placeholderImg} alt="" />
-              </div>
-              <Summary>
-                {stripHtmlFromString(episode.summary) || "No summary provided"}
-              </Summary>
-            </EpisodeListItem>
-          ))}
+          {response &&
+            response.map((episode) => (
+              <EpisodeListItem key={episode.id}>
+                <div>
+                  <h3> {episode.name}</h3>
+                  <p>Official Air Date: {formatDate(episode.airdate)}</p>
+                  <p>Runtime: {episode.runtime} minutes</p>
+                  <img src={episode.image?.medium || placeholderImg} alt="" />
+                </div>
+                <Summary>
+                  {stripHtmlFromString(episode.summary) ||
+                    "No summary provided"}
+                </Summary>
+              </EpisodeListItem>
+            ))}
         </Episodes>
       )}
       {error && <span>{error}</span>}
